affd13be1ee53d952b8f7d5b922950c61a4e077f
m0glan <m0glan@proton.me>
2022-07-31 18:29:24 +0200

    Implemented the SqliteConnectionEncryptionExtensions and added some tests.

--- M0glan.Sqlite.Utils.Test/TestUtils.cs
+++ M0glan.Sqlite.Utils.Test/TestUtils.cs
@@ -1,9 +1,44 @@
+using Microsoft.Data.Sqlite;
+
 namespace M0glan.Sqlite.Utils.Test;
 
 public static class TestUtils
 {
-    public static string GenerateTestFileName(string root, string extension)
+    public static string? GenerateTestFileName(string root, string extension)
     {
-        return $"{root}-{DateTimeOffset.UtcNow:yyyy-dd-M--HH-mm-ss}.{extension}";
+        if (string.IsNullOrWhiteSpace(extension))
+        {
+            throw new ArgumentNullException(nameof(extension));
+        }
+
+        string generated = $"{Guid.NewGuid()}-{DateTimeOffset.UtcNow:yyyy-dd-M--HH-mm-ss}";
+
+        if (string.IsNullOrWhiteSpace(root))
+        {
+            return Path.ChangeExtension(generated, extension);
+        }
+
+        return Path.ChangeExtension($"{root}-{generated}", extension);
+    }
+}
+
+public static class SqliteConnectionTestExtensions
+{
+    public static void Write(this SqliteConnection connection)
+    {
+        using SqliteCommand createTableCommand = connection.CreateCommand();
+        createTableCommand.CommandText = "CREATE TABLE TestTable (Id INT PRIMARY KEY)";
+        createTableCommand.ExecuteNonQuery();
+
+        using SqliteCommand insertRecordCommand = connection.CreateCommand();
+        insertRecordCommand.CommandText = "INSERT INTO TestTable VALUES (1)";
+        insertRecordCommand.ExecuteNonQuery();
+    }
+
+    public static async Task WriteAsync(this SqliteConnection connection)
+    {
+        using SqliteCommand command = connection.CreateCommand();
+        command.CommandText = "CREATE TABLE TestTable (Id INT PRIMARY KEY)";
+        await command.ExecuteNonQueryAsync();
     }
 }
--- M0glan.Sqlite.Utils/SqlCipherEncryption.cs
+++ M0glan.Sqlite.Utils/SqlCipherEncryption.cs
@@ -4,34 +4,9 @@ using Microsoft.Data.Sqlite;
 
 public class SqlCipherEncryption : ISqliteEncryption
 {
-    private const string READ_SQL = "SELECT COUNT(*) FROM sqlite_master";
-
     public void SetPassword(string dataSource, string currentPassword = "", string newPassword = "")
     {
-        if (string.IsNullOrWhiteSpace(dataSource))
-        {
-            throw new ArgumentNullException(nameof(dataSource));
-        }
-
-        if (!File.Exists(dataSource))
-        {
-            CreateWithPassword(dataSource, newPassword);
-        }
-
-        using var connection = new SqliteConnection(BuildConnectionString(dataSource, currentPassword, SqliteOpenMode.ReadWriteCreate));
-        connection.Open();
-        connection.SetPassword(newPassword);
-    }
-
-    private void CreateWithPassword(string dataSource, string password)
-    {
-        using var connection = new SqliteConnection(BuildConnectionString(dataSource, password, SqliteOpenMode.ReadWriteCreate));
-        connection.Open();
-    }
-
-    private void ChangePassword(string dataSource, string currentPassword, string newPassword)
-    {
-
+        throw new NotImplementedException();
     }
 
     public async Task SetPasswordAsync(string dataSource, string currentPassword = "", string newPassword = "", CancellationToken cancellationToken = default)
@@ -41,58 +16,16 @@ public class SqlCipherEncryption : ISqliteEncryption
 
     public bool TestPassword(string dataSource, string password = "")
     {
-        try
-        {
-            Read(BuildConnectionString(dataSource, password));
-            return true;
-        }
-        catch (SqliteException e)
-        {
-            if (e.ErrorCode == SqliteErrorCodes.NOT_A_DB)
-            {
-                return false;
-            }
-
-            throw;
-        }
-    }
-
-    private void Read(string connectionString)
-    {
-        using var connection = new SqliteConnection(BuildConnectionString(connectionString));
+        using var connection = new SqliteConnection(BuildConnectionString(dataSource, password));
         connection.Open();
-
-        using SqliteCommand command = connection.CreateCommand();
-        command.CommandText = READ_SQL;
-        command.ExecuteScalar();
+        return connection.Test();
     }
 
     public async Task<bool> TestPasswordAsync(string dataSource, string password = "", CancellationToken cancellationToken = default)
     {
-        try
-        {
-            await ReadAsync(BuildConnectionString(dataSource, password));
-            return true;
-        }
-        catch (SqliteException e)
-        {
-            if (e.ErrorCode == SqliteErrorCodes.NOT_A_DB)
-            {
-                return false;
-            }
-
-            throw;
-        }
-    }
-
-    private static async Task ReadAsync(string connectionString)
-    {
-        using var connection = new SqliteConnection(BuildConnectionString(connectionString));
+        using var connection = new SqliteConnection(BuildConnectionString(dataSource, password));
         await connection.OpenAsync();
-
-        using SqliteCommand command = connection.CreateCommand();
-        command.CommandText = READ_SQL;
-        await command.ExecuteScalarAsync();
+        return await connection.TestAsync();
     }
 
     private static string BuildConnectionString(string dataSource, string password = "", SqliteOpenMode mode = SqliteOpenMode.ReadOnly)
